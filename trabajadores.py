70dffe6af3fc06338bcd3f43a6ae8ed40c4fdeb4
camdejalapasta <jos.quezadar@duocuc.cl>
2024-06-14 12:01:51 -0400

    le faltan cosas

--- trabajadores.py
+++ trabajadores.py
@@ -1,17 +1,44 @@
 import os, time
-
+papus = []
 while True:
     os.system('cls')
     print("MENÚ TRABAJADORES")
     print("1. Registrar trabajador\n2. Listar todos los trabajadores\n3. Imprimir planilla de sueldos\n 4. Sair del programa")
-    opc = int(input("Ingrese opción"))
+    opc = int(input("Ingrese opción: "))
     os.system('cls')
     if opc==1:
-        pass
+        print("REGISTRAR TRABAJADOR")
+        nombre_apellido = input("Ingrese nombre y apellido: ")
+        cargo = int(input("Ingrese cargo(1:CEO, 2:DESARROLLADOR, 3:ANALISTA): "))
+        sueldo_solido = int(input("Ingrese sueldo bruto: "))
+        desc_salud = int(7/100 * sueldo_solido)
+        desc_afp = int(12/100 * sueldo_solido)
+        sueldo_liquido = sueldo_solido-desc_afp-desc_salud
+        papu = [nombre_apellido,cargo,sueldo_solido,desc_salud,desc_afp]
+        papus.append(papu)
+        print("TRABAJADOR REGISTRADO CON ÉXITO!")
+ 
     elif opc==2:
-        pass
+        if len(papus)==0:
+            print("No hay papus :,v")
+        else:
+            print("\tLISTA DE TRABAJADORES")
+            print("Trabajador\tCargo\tSueldo Bruto\tDesc. Salud\tDesc. AFP\tLíquido")
+            for t in papus:
+                 print(f"{t[0]}\t{t[1]}\t{t[2]}\t\t\t{t[3]}\t\t\t{t[4]}\t{t[5]}")
+
+
     elif opc==3:
-        pass
+        if len(papus)==0:
+            print("No existen trabajadores, elija la opción 1")
+        else:
+            opc2 = int(input("¿Qué cargo desea imprimit(1:CEO, 2:DESARROLLOR 3:ANALISTA)"))
+            if opc2 ==4:
+                with open("todos_trabajadores.txt","w", newline="\n") as archivo:
+                    for t in papus:
+                        texto =  f"{t[0]} {t[1]} {t[2]} {t[3]} {t[4]} {t[5]}"
+                        archivo.write(texto)
+
     else:
         print("Gracias por usar el programa, bye bye")
         break
