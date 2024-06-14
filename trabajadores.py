63daf599dc82905f1a75f8db376d71cf8e9c4c01
camdejalapasta <jos.quezadar@duocuc.cl>
2024-06-14 11:05:29 -0400

    ejercicio menu beta

---
+++ trabajadores.py
@@ -0,0 +1,18 @@
+import os, time
+
+while True:
+    os.system('cls')
+    print("MENÚ TRABAJADORES")
+    print("1. Registrar trabajador\n2. Listar todos los trabajadores\n3. Imprimir planilla de sueldos\n 4. Sair del programa")
+    opc = int(input("Ingrese opción"))
+    os.system('cls')
+    if opc==1:
+        pass
+    elif opc==2:
+        pass
+    elif opc==3:
+        pass
+    else:
+        print("Gracias por usar el programa, bye bye")
+        break
+    time.sleep(3)
